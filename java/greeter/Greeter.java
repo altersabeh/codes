372f096ea9a85c465b8881032d1fd7b3fd592cf3
altersabeh <seriotigrillo@gmail.com>
2023-10-30 17:56:33 +0800

    added java greeter

---
+++ java/greeter/Greeter.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Greeter {
+  private String username;
+
+  public Greeter(String name) {
+    this.username = name;
+  }
+
+  public void greet() {
+    System.out.println("Hello, " + this.username + "!");
+    System.out.println("Greetings from Java!");
+  }
+
+  public static class InputHandler {
+    public static String getUserName() {
+      Scanner scanner = new Scanner(System.in);
+      System.out.print("Enter your name: ");
+      String userinput = scanner.nextLine();
+
+      String name = userinput.trim();
+
+      return name;
+    }
+  }
+
+  public static void main(String[] args) {
+    String name = InputHandler.getUserName();
+    Greeter greeter = new Greeter(name);
+
+    greeter.greet();
+  }
+}
