b73cf54c5f7f6e86193b16cb356312ba45cc671a
altersabeh <seriotigrillo@gmail.com>
2023-10-30 17:52:33 +0800

    changed main name

--- java/hello/Hello.java
+++ java/hello/Hello.java
@@ -1,33 +1,7 @@
-import java.util.Scanner;
+// Hello World in Java !
 
-public class Greeter {
-  private String username;
-
-  public Greeter(String name) {
-    this.username = name;
-  }
-
-  public void greet() {
-    System.out.println("Hello, " + this.username + "!");
-    System.out.println("Greetings from Java!");
-  }
-
-  public static class InputHandler {
-    public static String getUserName() {
-      Scanner scanner = new Scanner(System.in);
-      System.out.print("Enter your name: ");
-      String userinput = scanner.nextLine();
-
-      String name = userinput.trim();
-
-      return name;
-    }
-  }
-
+public class Hello {
   public static void main(String[] args) {
-    String name = InputHandler.getUserName();
-    Greeter greeter = new Greeter(name);
-
-    greeter.greet();
+    System.out.println("Hello World from Java !");
   }
 }
