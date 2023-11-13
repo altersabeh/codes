3515933d728cb7741237e67a080812c3488c6439
altersabeh <seriotigrillo@gmail.com>
2023-11-13 16:28:03 +0800

    added dart programs

---
+++ dart/fibonacci/bin/dart_fibonacci.dart
@@ -0,0 +1,5 @@
+import 'package:dart_fibonacci/fibonacci.dart' as fibonacci;
+
+void main() {
+  fibonacci.main();
+}
---
+++ dart/hello/bin/dart_hello.dart
@@ -0,0 +1,5 @@
+import 'package:dart_hello/hello.dart' as hello;
+
+void main() {
+  hello.main();
+}
