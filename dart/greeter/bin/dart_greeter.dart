a94cc065aa359bdd2ed55324da136f35a826f7e1
altersabeh <seriotigrillo@gmail.com>
2023-11-13 23:23:35 +0800

    added dart greeter program

---
+++ dart/greeter/bin/dart_greeter.dart
@@ -0,0 +1,5 @@
+import 'package:dart_greeter/greeter.dart' as greeter;
+
+void main() {
+  greeter.main();
+}
---
+++ dart/greeter/lib/greeter.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+
+class InputHandler {
+  static String? getUserName() {
+    stdout.write("Enter your name: ");
+    String? userInput = stdin.readLineSync();
+
+    String? name = userInput?.trim();
+
+    return name;
+  }
+}
+
+class Greeter {
+  String username;
+
+  Greeter(this.username);
+
+  void greet() {
+    print("Hello, $username!");
+    print("Greetings from Dart!");
+  }
+}
+
+void main() {
+  String? name = InputHandler.getUserName();
+  Greeter greeter = Greeter(name!);
+
+  greeter.greet();
+}
