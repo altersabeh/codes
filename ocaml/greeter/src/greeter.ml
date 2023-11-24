ab393324b250ed21f209b37d1806bc92fa30fef3
altersabeh <seriotigrillo@gmail.com>
2023-11-24 17:05:03 +0800

    improved ocaml greeter program

--- ocaml/greeter/src/greeter.ml
+++ ocaml/greeter/src/greeter.ml
@@ -1,6 +1,27 @@
-(* Hello World from OCaml ! *)
+module InputHandler = struct
+  let get_user_name () =
+    print_string "Enter your name: ";
+    let userinput = read_line () in
+    String.trim userinput
+end
+
+module Greeter = struct
+  type username = string option
+  type t = { username : username }
+
+  let create name = { username = name }
+
+  let get_greeting_message = function
+    | Some name when name <> "" -> "Hello, " ^ name ^ "!"
+    | _ -> "Hello, Stranger!"
+
+  let greet greeter =
+    let greeting = get_greeting_message greeter.username in
+    Printf.printf "%s\n" greeting;
+    print_endline "Greetings from OCaml!"
+end
 
 let () =
-  print_string "Please enter your name :";
-  let user_input = read_line () in
-  Printf.printf "Hello, %s!\n" user_input
+  let name = InputHandler.get_user_name () in
+  let greeter = Greeter.create (Some name) in
+  Greeter.greet greeter
