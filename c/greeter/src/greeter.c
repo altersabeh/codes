c005e55e4db44d2da5fae0396960e01eba7f2cf3
altersabeh <seriotigrillo@gmail.com>
2023-12-19 17:20:36 +0800

    updated greeter code in c

--- c/greeter/src/greeter.c
+++ c/greeter/src/greeter.c
@@ -1,8 +1,46 @@
-// Hello World in C !
-
+#include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* GetUserName() {
+  char userinput[100];
+  printf("Enter your name: ");
+  if (fgets(userinput, 100, stdin) == NULL) {
+    return NULL;
+  }
+
+  char* name = strdup(userinput);
+  name[strlen(name)-1] = '\0';
+
+  name += strspn(name, " \t\n\r");
+  size_t len = strlen(name);
+  while (len > 0 && isspace((unsigned char)name[len - 1])) {
+    name[--len] = '\0';
+  }
+
+  if (strlen(name) == 0) {
+    strcpy(name, "Stranger");
+  }
+
+  return name;
+}
+
+typedef struct {
+  char* username;
+} Greeter;
+
+void Greeter_Greet(Greeter* greeter) {
+  printf("Hello, %s!\n", greeter->username);
+  puts("Greetings from C!");
+}
 
 int main() {
-  printf("Hello World from C !\n");
+  char* name = GetUserName();
+  Greeter greeter;
+  greeter.username = name;
+
+  Greeter_Greet(&greeter);
+
   return 0;
 }
