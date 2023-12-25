eb2e1f478a866e55525e53a8ff4f9a709c527844
altersabeh <seriotigrillo@gmail.com>
2023-12-25 18:58:02 +0800

    improved greeter program

--- java/greeter/src/main/java/Greeter.java
+++ java/greeter/src/main/java/Greeter.java
@@ -2,25 +2,25 @@ package greeter;
 
 import java.util.Scanner;
 
-public class Greeter {
-  public static class InputHandler {
-    public static String getUserName() {
-      Scanner scanner = new Scanner(System.in);
-      System.out.print("Enter your name: ");
-      String userinput = scanner.nextLine();
+class InputHandler {
+  public static String getUserName() {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Enter your name: ");
+    String userinput = scanner.nextLine();
 
-      String name = userinput.trim();
+    String name = userinput.trim();
 
-      if (name.isEmpty()) {
-        name = "Stranger";
-      }
-
-      scanner.close();
-
-      return name;
+    if (name.isEmpty()) {
+      name = "Stranger";
     }
-  }
 
+    scanner.close();
+
+    return name;
+  }
+}
+
+public class Greeter {
   private String username;
 
   public Greeter(String name) {
