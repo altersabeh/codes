35dcc1b41d3086083c623c2c66c0969335cd8f58
altersabeh <betamaximum22@gmail.com>
2024-01-01 22:36:05 +0800

    added dialyzer

--- mix.exs
+++ mix.exs
@@ -7,11 +7,13 @@ defmodule Codes.MixProject do
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       build_path: "dist/elixir",
-      deps_path: "dist/elixir/deps",
+      deps_path: "dist/elixir/deps"
     ]
   end
 
   defp deps do
-    []
+    [
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false}
+    ]
   end
 end
