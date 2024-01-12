fe07a9d96297c67ef962eac2751903d67c8527c9
altersabeh <betamaximum22@gmail.com>
2024-01-12 16:36:05 +0800

    added greeter program in scala

--- build.sbt
+++ build.sbt
@@ -1,3 +1,4 @@
-lazy val root = (project in file(".")).aggregate(hello)
+lazy val root = (project in file(".")).aggregate(hello, greeter)
 
 lazy val hello = project.withId("scala-hello").in(file("scala/hello"))
+lazy val greeter = project.withId("scala-greeter").in(file("scala/greeter"))
---
+++ scala/greeter/src/main/scala/Greeter.scala
@@ -0,0 +1,23 @@
+package greeter
+
+import scala.io.StdIn._
+
+object InputHandler:
+  def getUserName(): String =
+    print("Enter your name: ")
+    val userInput = readLine()
+
+    val name = userInput.trim
+
+    if name.isEmpty then "Stranger" else name
+
+class Greet(name: String):
+  def greet(): Unit =
+    println(s"Hello, $name!")
+    println("Greetings from Scala!")
+
+@main def Greeter(): Unit =
+  val name = InputHandler.getUserName()
+  val greeter = new Greet(name)
+
+  greeter.greet()
