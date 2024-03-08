c9fa8b0f7128703994459abce897112e3cef9802
AlterSabeh <betamaximum22@gmail.com>
2024-03-08 23:00:01 +0800

    fixed formatting

--- java/message/src/main/java/message/Message.java
+++ java/message/src/main/java/message/Message.java
@@ -2,9 +2,11 @@ package message;
 
 public class Message {
   private static String globalMessage = "This message is from global variable !";
+
   private static void printMessage() {
     System.out.println("This message is from private method !");
   }
+
   public static void main(String[] args) {
     String localMessage = "This message is from local variable !";
     System.out.println(localMessage);
